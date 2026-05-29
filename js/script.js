dde603366fe1ae6eb5a101d16cf39d8beb15ab9d
sheastanton <sheastanton07@gmail.com>
2026-05-29 19:41:10 -0400

    edits
    v3

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 80) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
